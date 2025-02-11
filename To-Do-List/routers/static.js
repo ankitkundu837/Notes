b48e5c6342d34afd698b59eaa79a3b30cc45877a
kumro-potash <goswamineelim@outlook.com>
2025-02-11 18:28:32 +0530

    Add files via upload

---
+++ To-Do-List/routers/static.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const router = express.Router();
+
+router.get('/', async(req,res)=>{
+    if(!req.user) return res.redirect('/login')
+    return res.render("home")
+})
+
+router.get('/signup', async(req, res)=>{
+    return res.render("signup");
+})
+
+router.get('/login', async(req, res)=>{
+    return res.render("login");
+})
+
+module.exports = router;
